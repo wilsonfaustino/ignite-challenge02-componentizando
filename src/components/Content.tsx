9f48e05a5c4bb51551f4484c0cfd2a3bc898482a
Wilson Faustino <wilson.rfaustino@gmail.com>
2021-04-02 16:16:10 -0300

    :tada: Challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,22 @@
-export function Content() {
-  // Complete aqui
+import { ReactNode } from "react";
+
+interface ContentProps {
+  title: string;
+  children: ReactNode;
+}
+
+export function Content({children, title}: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {children}
+          </div>
+        </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+import { ReactNode } from "react";
+
+interface SideBarProps {
+  children: ReactNode;
+}
+
+export function SideBar({children}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {children}
+        </div>
+
+      </nav>
+  )
 }
